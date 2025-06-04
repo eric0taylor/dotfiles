ceb66747ba7be4d48d2ab77a29e6b8cbb3fec575
Eric Taylor <blister0exist@yandex.ru>
2025-06-04 17:26:18 +0300

    install scripts move to main

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+for install:
+```bash
+sh -c "$(wget -O- https://raw.githubusercontent.com/eric0taylor/dotfiles/refs/heads/main/dotfiles.sh)
+```
---
+++ install_scripts/slinux_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+###########################################################
+### Install dotfiles script                             ###
+### Author: Eric Taylor <blister0exist@yandex.ru>       ###
+### Repo: <https://github.com/eric0taylor/dotfiles.git> ###
+### License: MIT                                        ###
+###########################################################
