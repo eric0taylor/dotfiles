6d3642ba9aed1f6215a61e438cddf7540b26a64a
Eric Taylor <blister0exist@yandex.ru>
2025-06-06 17:26:39 +0300

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,12 +16,15 @@ apt-get update && apt-get dist-upgrade -y && apt-get install -y update-kernel &&
 # TODO add reboot and "remove-old-kernels -y"
 
 # install soft
-apt-get install -y xorg-drv-video xorg-drv-input ly i3 rofi alacritty far2l fonts-ttf-gnu-freefont-mono git sudo wget
+apt-get install -y xorg-drv-video xorg-drv-input ly i3 rofi alacritty far2l fonts-ttf-gnu-freefont-mono git sudo wget zsh
 # set services
 systemctl enable ly
 systemctl set-default graphical.target
 # set sudo settings
 sed -i 's/# root ALL/root ALL/g' /etc/sudoers
+# set zsh for root and user
+chsh -s /bin/zsh ivan
+chsh -s /bin/zsh
 
 # set theme
 mkdir -p /usr/share/themes/Windows-95
@@ -31,5 +34,6 @@ rm -rfv /usr/share/themes/Windows-95/{cinnamon,gnome-shell,LICENSE,metacity-1,un
 mkdir /home/$USERNAME/.local/bin/
 wget -O $MANAGE_SCRIPT $SCRIPT_URL 
 chown $USERNAME:$USERNAME $MANAGE_SCRIPT && chmod +x $MANAGE_SCRIPT
-sudo -u $USERNAME $MANAGE_SCRIPT install
-ln -sf /home/$USERNAME/.config/ly/config.ini /etc/ly/config.ini
+echo "alias dotmanage=\"/home/$USERNAME/.local/manage.sh\"" >> /home/$USERNAME/.zshrc
+echo "Login on $USERNAME and do \"dotmanage install\""
+echo "Success install. Exit..."
--- scripts/manage.sh
+++ scripts/manage.sh
@@ -8,6 +8,7 @@
 # Variables
 REPO_PATH="https://github.com/eric0taylor/dotfiles.git"
 _dotfiles="/usr/bin/git --git-dir="$HOME/.dotfiles" --work-tree="$HOME""
+GITKEY="$HOME/.ssh/github-key"
 
 # Check git settings
 if [[ -f $HOME/.gitconfig ]]; then
@@ -20,17 +21,23 @@ fi
 # Check action
 case "$1" in
   install)
+    # Check installing
     if [[ -d "$HOME/.dotfiles" ]];then
       echo "Dotfiles is installed! Use "update". Exit..."
       exit 0
     else
+      # Clone repo and do settings
       git clone --bare $REPO_PATH $HOME/.dotfiles
       $_dotfiles checkout
       $_dotfiles config --local status.showUntrackedFiles no
+      # set links
+      ln -sf /home/$USERNAME/.config/ly/config.ini /etc/ly/config.ini
     fi
     ;;
 
   update)
+  # Check repo and update dotfiles
+  # NEED SSH KEY FOR GITHUB
     if [[ -d "$HOME/.dotfiles" ]];then
       $_dotfiles checkout
       $_dotfiles pull origin
@@ -41,14 +48,25 @@ case "$1" in
     ;;
 
   push)
-    read -r -p "Enter commit messege: "
-    $_dotfiles commit -m $REPLY
-    $_dotfiles push origin
-    exit 0
+  # Chenk repo, commit and puch to github
+    if [[ -d "$HOME/.dotfiles" && -f $GITKEY ]];then
+      read -r -p "Enter commit messege: "
+      $_dotfiles commit -m $REPLY
+      $_dotfiles push origin
+      exit 0
+    else
+      echo "Dotfiles or github ssh key not exist! Use "install" and add github ssh key. Exit..."
+      exit 1
     ;;
 
   help)
-    echo "Script for manage dotfiles\nSyntax: dotfiles.sh <action>\n  <action>:\n    install: installing dotfiles\n    update: updating dotfiles\n    push: upload new configs to github\n    help: show this messege"
+    echo "Script for manage dotfiles"
+    echo "Syntax: dotfiles.sh <action>"
+    echo "  <action>:"    
+    echo "    install: installing dotfiles"
+    echo "    update: updating dotfiles"
+    echo "    push: upload new configs to github (need github ssh key: ~/.ssh/github-key)"
+    echo "    help: show this messege"
     exit 0
     ;;
     
